c6fe6cebbd21993cb6553a3e81de75c884b25ab3
gcampes <gabrieldecampes@gmail.com>
2016-03-31 10:25:23 -0300

    Changes authorizer cookie fetch logic

--- addon/authorizers/imdt.js
+++ addon/authorizers/imdt.js
@@ -1,16 +1,26 @@
 import Ember from 'ember';
 import Base from 'ember-simple-auth/authorizers/base';
 
-const { isEmpty } = Ember;
+const {
+  isEmpty
+} = Ember;
+
+const getCookie = function(name) {
+  var value = "; " + document.cookie;
+  var parts = value.split("; " + name + "=");
+  if (parts.length === 2) {
+    return parts.pop().split(";").shift();
+  }
+};
 
 export default Base.extend({
   tokenAttributeName: 'session_token',
 
   authorize(data, block) {
     const tokenAttributeName = this.get('tokenAttributeName');
-    const userToken          = data[tokenAttributeName];
+    const userToken = data[tokenAttributeName];
 
-    if (!isEmpty(userToken) && !document.cookie) {
+    if (!isEmpty(userToken) && getCookie('PD-S-SESSION-ID')) {
       block('Authorization', `Token ${userToken}`);
     }
   }
